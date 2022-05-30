66b31c05fd2d2abfb85bf16e616a183aaa161682
Bruno Andrade <brunoan99@gmail.com>
2022-05-30 11:29:25 -0300

    refactor: change test method to be more simply

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -16,13 +16,13 @@ describe('EmailValidator Adapter', () => {
     const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
-    expect(isValid).toBe(false)
+    expect(isValid).toBeFalsy()
   })
 
   test('Should return true if validator return true', () => {
     const sut = makeSut()
     const isValid = sut.isValid('valid_email@mail.com')
-    expect(isValid).toBe(true)
+    expect(isValid).toBeTruthy()
   })
 
   test('Should call validator with correct email', () => {
