280339a144d8e9fca9d6c19b63d6eba1e71b6407
Bruno Andrade <brunoan99@gmail.com>
2022-06-21 07:30:52 -0300

    test: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -7,10 +7,21 @@ describe('Login Controller', () => {
     const sut = new LoginController()
     const httpRequest = {
       body: {
-        password: '123456'
+        password: 'any_password'
       }
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParameterError('email')))
+  })
+
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParameterError('password')))
   })
 })
