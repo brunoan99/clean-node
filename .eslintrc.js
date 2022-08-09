636d6804971465f9033038b00b773a9fc9c59b61
Bruno Andrade <brunoan99@gmail.com>
2022-08-09 19:09:30 -0300

    chore: add rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,22 +16,12 @@ module.exports = {
     '@typescript-eslint'
   ],
   'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'lines-between-class-members': ['warn', 'always'],
+    'no-multiple-empty-lines': ['warn', {max: 1, maxEOF: 0, maxBOF: 0}],
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any': 'off'
   }
